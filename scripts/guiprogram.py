7aae263e8f3c0180ed04462c5b3dd87a87a247de
Jeff Jewett <jeffjewett27@gmail.com>
2021-10-11 18:41:05 -0400

    gui fix

--- scripts/guiprogram.py
+++ scripts/guiprogram.py
@@ -1,6 +1,7 @@
 from drone.tello import Tello
 from gui.simplegui import GUI
 from video.videofeed import VideoFeed
+from gui.videocomponent import VideoComponent
 from controller.simplecontroller import SimpleController
 
 print("started")
@@ -9,7 +10,8 @@ tello.connect()
 print("video")
 video = VideoFeed(tello)
 print("gui")
-gui = GUI(video)
+video_component = VideoComponent(video)
+gui = GUI((video_component,))
 print("controller")
 controller = SimpleController(tello, gui)
 gui.run()
